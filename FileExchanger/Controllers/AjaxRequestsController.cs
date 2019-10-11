5b2b5c0c03eb4d4fe439544efa88fc5c5c58ee34
Rafayel <Rafayel@DESKTOP-3A2MHUU>
2019-10-11 12:10:11 +0400

    Messages create

---
+++ FileExchanger/Controllers/AjaxRequestsController.cs
@@ -0,0 +1,18 @@
+﻿using FileExchanger.Data;
+using FileExchanger.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileExchanger.Controllers
+{
+	public class AjaxRequestsController : Controller
+    {
+		private readonly ApplicationDbContext _context;
+		private readonly UserManager<User> _userManager;
+		public AjaxRequestsController(ApplicationDbContext context, UserManager<User> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
+    }
+}
